b23c302dbd3c3e633a52f3bc4cdabee9a62e3be7
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 19:26:58 +0300

    correction made in class name

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,7 +11,7 @@ const Form = ({setInputText}) => {
                 <button className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
                 </button>
-                    <div class="select">
+                    <div className="select">
                         <select name="todos" className="filter-todo">
                             <option value="all">All</option>
                             <option value="completed">Completed</option>
