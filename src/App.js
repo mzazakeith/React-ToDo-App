91c001be80665a5db2e56ef8b54f815770fd18e7
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 19:51:42 +0300

    adding props and add an onClick handler for the submit button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import TodoList from './components/TodoList';
 import {useState} from 'react'
 
 function App() {
-  const[inputText, setInputText] = useState("")
+  const[inputText, setInputText] = useState("");
+  const[todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Simple ToDo List</h1>
       </header>
-      <Form setInputText={setInputText}/>
+      <Form inputText = {inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} />
       <TodoList/>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-const Form = ({setInputText}) => {
+const Form = ({setInputText, todos, setTodos}) => {
     const inputTextHandler = (e) => {
         console.log(e.target.value);
         setInputText(e.target.value);
-    }
+    };
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+    } 
     return(
         <div>
             <form>
                 <input type="text" onChange={inputTextHandler} className="todo-input" />
-                <button className="todo-button" type="submit">
+                <button className="todo-button" type="submit" onClick={submitTodoHandler}>
                 <i className="fas fa-plus-square"></i>
                 </button>
                     <div className="select">
