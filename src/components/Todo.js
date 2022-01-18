4dd2c55c5a4fc67f4bb1292dcd635c017f7acd2f
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 22:25:34 +0300

    logic added for css class when a task is completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -12,7 +12,7 @@ const Todo= ({todo, todos, setTodos, text}) => {
         setTodos(todos.map(item => {
             if(item.id === todo.id){
                 return produce(item, completeItem => {
-                    completeItem.completed = true;
+                    completeItem.completed = !completeItem.completed;
                 })
             }
             return item;
@@ -22,7 +22,7 @@ const Todo= ({todo, todos, setTodos, text}) => {
 
     return(
         <div className="todo">
-            <li className="todo-item">{text}</li>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
             <button onClick={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
