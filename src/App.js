f0db5f3ca44a139b576c5b49ce3b6eb38716a358
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 18:52:08 +0300

    form imported to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
@@ -6,7 +7,7 @@ function App() {
       <header>
         <h1>Simple ToDo List</h1>
       </header>
-      
+      <Form/>
     </div>
   );
 }
